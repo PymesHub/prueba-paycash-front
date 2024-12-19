8cfb9a8c7b777efa582ef9ac6fa4d0ad8ff6a8ae
Carlos jarero <pymeshub19@gmail.com>
2024-12-19 15:29:14 -0600

    Resolve String Error

--- src/components/TableUsers/components/BirthdayDetail/BirthdayDetail.tsx
+++ src/components/TableUsers/components/BirthdayDetail/BirthdayDetail.tsx
@@ -5,7 +5,7 @@ const BirthdayDetail: React.FC<{ user: UserModel }> = ({ user }) => {
   return (
     <div className="flex flex-col gap-1">
       <p className="text-bold text-sm capitalize text-default-400">
-        {formatDate(user.birthday)}
+        {formatDate(String(user.birthday))}
       </p>
     </div>
   );
